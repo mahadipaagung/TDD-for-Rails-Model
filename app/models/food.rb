3ede9baeae319eec36434d8fa98dad6e169fab75
mahadipaagung <mahadipa.agung@gmail.com>
2022-04-14 15:23:20 +0000

    Add validation of numeric and price greater than 0.01

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,2 +1,8 @@
 class Food < ApplicationRecord
+  validates :name, presence: true, uniqueness: true
+  validates :price, numericality:{greater_than_or_equal_to: 0.01}
+
+  def self.by_letter(letter)
+    where("name LIKE ?", "#{letter}%").order(:name)
+  end
 end
